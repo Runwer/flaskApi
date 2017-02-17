aefb08e1a7ab00ac0c019746f62769e15eb1b9ec
Runwer <runewerliin@runes-mbp.home>
2017-02-17 08:06:49 +0100

    Mongo setup and playing with cookies

--- app/dbDriver.py
+++ app/dbDriver.py
@@ -5,7 +5,7 @@ client = MongoClient('localhost', 27017)
 
 db = client.moviesdb
 
-def findmov():
+def findMovBattleRand():
     query = [{"$sample": {"size": 2}}]
     projection = [{"_id": 0, "Title": 1, "Poster": 1, "id":1}]  # 0 kommer ikke med, 1 kommer med
     movs = db.moviesCol.aggregate(query)
@@ -14,12 +14,40 @@ def findmov():
         out.append(m)
     return JSONEncoder().encode(out)
 
-#def insertmovies():
-#    for m in movies:
-#        db.moviesCol.insert(m)
+def findMovBattleVs(vs):
+    query = [{"$match":
+        {"id": {"$ne": vs}} },
+    {"$sample": {"size": 1}}]
+    projection = [{"_id": 0, "Title": 1, "Poster": 1, "id":1}]  # 0 kommer ikke med, 1 kommer med
+    movs = db.moviesCol.aggregate(query)
+    out = []
+    for m in movs:
+        out.append(m)
+    return JSONEncoder().encode(out)
+
+def findMov(id):
+    query = {"id":id}
+    projection = {"_id": 0, "Title": 1, "Poster": 1, "id": 1}
+    return db.moviesCol.find_one(query, projection)
+
+#def findList(idlist):
+#    query = [{"id":{"$in":idlist}}]
+#    projection = [{"_id": 0, "Title": 1, "Poster": 1, "id": 1}]
+
+
+def insertEdge(edge):
+    db.edges.insert(edge)
+
+def findEdge(uid, id):
+    query = {"uid":uid, "head": id}
+    projection = {"_id": 0, "uid":1}
+    return db.edges.find(query, projection).count()
+
+
+
 
 if __name__ == '__main__':
-    print {'movies':findmov()}
+    print findEdge("1", "250")
 
 
 
--- app/views.py
+++ app/views.py
@@ -5,7 +5,9 @@ import random
 from flask_cors import CORS, cross_origin
 from Pagerank import pagerank
 import operator
-from dbDriver import findmov
+from dbDriver import findMovBattleRand, findMovBattleVs, findMov
+
+uid = "1"
 
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
@@ -17,18 +19,6 @@ def index():
     return "Hello, World!"
 
 
-movies = [
-    {"id": "0", "Title":"Interstellar", "Poster": "Interstellar", "Year":"2014" },
-    {"id": "1", "Title":"Blade Runner", "Poster": "BladeRunner", "Year": "1982" },
-    {"id": "2", "Title": "The Hobbit: the Battle of the Five Armies", "Poster": "TheHobbit_TheBattleoftheFiveArmies", "Year": "2014"},
-     {"id": "3", "Title":"Metropolis", "Poster": "Metropolis", "Year": "1929" },
-     {"id": "4", "Title":"Alien", "Poster": "Alien", "Year": "1979" },
-    {"id": "5", "Title":"Gangs of Wasseypur", "Poster": "GangsofWasseypur", "Year": "2012", "Description": "A clash between Sultan and Shahid Khan leads to the expulsion of Khan from Wasseypur, and ignites a deadly blood feud spanning three generations." },
-     {"id": "6", "Title":"Sin City", "Poster": "SinCity","Year":	"2005", "Description":	"A film that explores the dark and miserable town, Basin City, and tells the story of three different people, all caught up in violent corruption."},
-    {"id": "7",	"Title":"Kind Hearts and Coronets","Poster": "KindHeartsandCoronets",	"Year":	"1949", "Description":	"A distant poor relative of the Duke of D'Ascoyne plots to inherit the title by murdering the eight other heirs who stand ahead of him in the line of succession."},
-    {"id": "8",	"Title":"Song of the Sea", "Poster": "SongoftheSea",	"Year":	"2014", "Description":	"Ben, a young Irish boy, and his little sister Saoirse, a girl who can turn into a seal, go on an adventure to free the faeries and save the spirit world."}
-]
-
 ids = [0,1,2,3,4,5,6,7,8]
 edges = []
 
@@ -37,11 +27,11 @@ edges = []
 def moviedb():
     moviecount = int(request.args.get('count'))
     if moviecount == 1:
-        mov = int(request.args.get('vsmovie'))
-        mov1 = random.choice(list(set(ids)-set([mov])))
-        return jsonify({'movies': [movies[mov1]]})
+        #Consider doing a double call, then see if id of first == id of vsmovie
+        mov = request.args.get('vsmovie')
+        return findMovBattleVs(mov)
     if moviecount == 2:
-        return findmov()
+        return findMovBattleRand()
 
 @app.route('/moviedb/api/v1.0/edge', methods=['POST'])
 @cross_origin()
@@ -59,10 +49,11 @@ def toplist():
         tlist = pagerank(edges)[0]
         sortedtlist = sorted(tlist.items(), key=operator.itemgetter(1), reverse=True)
         outmovs = []
-        for mov in sortedtlist:
-            outmovs.append({'id': mov[0], 'points': ("%.2f" % mov[1]), 'Title': movies[int(mov[0])]['Title'],
-                            'Poster': movies[int(mov[0])]['Poster']
-            })
+        for movlis in sortedtlist:
+            temp = findMov(movlis[0])
+            temp["points"] = ("%.2f" % movlis[1])
 
-        return jsonify({'list': outmovs})
+            outmovs.append(temp)
+
+        return jsonify(outmovs)
     else: return jsonify({'list': []})
