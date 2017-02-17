90f528d7b632bac23da3f674c32f62d2d74fc473
Runwer <runewerliin@runes-mbp.home>
2017-02-18 00:01:04 +0100

    Final API call. Now refactor and cookie call needed

--- app/dbDriver.py
+++ app/dbDriver.py
@@ -5,8 +5,12 @@ client = MongoClient('localhost', 27017)
 
 db = client.moviesdb
 
-def findMovBattleRand():
-    query = [{"$sample": {"size": 2}}]
+
+## functions that mess with moviesCol
+def findMovBattleRand(notlist):
+    query = [{"$match":
+        {"id": {"$nin": notlist}} },
+             {"$sample": {"size": 2}}]
     projection = [{"_id": 0, "Title": 1, "Poster": 1, "id":1}]  # 0 kommer ikke med, 1 kommer med
     movs = db.moviesCol.aggregate(query)
     out = []
@@ -16,7 +20,7 @@ def findMovBattleRand():
 
 def findMovBattleVs(vs):
     query = [{"$match":
-        {"id": {"$ne": vs}} },
+        {"id": {"$nin": vs}} },
     {"$sample": {"size": 1}}]
     projection = [{"_id": 0, "Title": 1, "Poster": 1, "id":1}]  # 0 kommer ikke med, 1 kommer med
     movs = db.moviesCol.aggregate(query)
@@ -30,24 +34,50 @@ def findMov(id):
     projection = {"_id": 0, "Title": 1, "Poster": 1, "id": 1}
     return db.moviesCol.find_one(query, projection)
 
-#def findList(idlist):
-#    query = [{"id":{"$in":idlist}}]
-#    projection = [{"_id": 0, "Title": 1, "Poster": 1, "id": 1}]
 
-
+## functions that plays around with the Collection of edges
 def insertEdge(edge):
     db.edges.insert(edge)
 
-def findEdge(uid, id):
-    query = {"uid":uid, "head": id}
-    projection = {"_id": 0, "uid":1}
-    return db.edges.find(query, projection).count()
+def findEdge(uid):
+    query = {"user":uid}
+    projection = {"_id": 0, "win": 1, "loose": 1}
+    edgesDict = db.edges.find(query, projection)#.count()
+    edges = []
+    for e in edgesDict:
+        edges.append([str(e["loose"]), str(e["win"])])
+    return edges
 
+def pctEdge(movid1, movid2):
+    query = {"win":str(movid1), "loose": str(movid2)}
+    w1 = db.edges.find(query).count()
+    query = {"win":movid2, "loose": movid1}
+    w2 = db.edges.find(query).count()
+    if w1+w2 != 0:
+        out1 = w1 * 100 / (w1 + w2)
+        pctout = [out1, 100-out1]
+        return pctout
+    else:
+        return [movid1,movid2]
+
+### functions regarding the notSeen Collection
+def notSeen(uid, movid):
+    db.notSeenCol.insert({"uid": uid, "movieID": movid})
+
+def getNotSeen(uid):
+    query = {"user": uid}
+    projection = {"_id": 0, "movieID": 1}
+    outq = db.notSeenCol.find(query, projection)
+    outlist = []
+    for q in outq:
+        outlist.append(q["movieID"])
+    return outlist
 
 
 
 if __name__ == '__main__':
-    print findEdge("1", "250")
+    for e in db.edges.find():
+        print e
 
 
 
--- app/views.py
+++ app/views.py
@@ -5,46 +5,41 @@ import random
 from flask_cors import CORS, cross_origin
 from Pagerank import pagerank
 import operator
-from dbDriver import findMovBattleRand, findMovBattleVs, findMov
+from dbDriver import findMovBattleRand, findMovBattleVs, findMov, insertEdge, findEdge, pctEdge, notSeen, getNotSeen
 
-uid = "1"
+uid = "5"
 
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 #app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
-
-
-ids = [0,1,2,3,4,5,6,7,8]
-edges = []
-
+#Get movies for Versus
 @app.route('/moviedb/api/v1.0/movies', methods=['GET'])
 @cross_origin()
 def moviedb():
     moviecount = int(request.args.get('count'))
     if moviecount == 1:
         #Consider doing a double call, then see if id of first == id of vsmovie
-        mov = request.args.get('vsmovie')
+        mov = getNotSeen(uid)
+        mov.append(request.args.get('vsmovie'))
+        notSeen(uid, request.args.get('dump'))
         return findMovBattleVs(mov)
     if moviecount == 2:
-        return findMovBattleRand()
+        return findMovBattleRand(getNotSeen(uid))
 
 @app.route('/moviedb/api/v1.0/edge', methods=['POST'])
 @cross_origin()
 def create_task():
     if not request.json or not 'win' in request.json:
         abort(400)
-    newEdge = [request.json['loose'], request.json['win']]
-    edges.append(newEdge)
-    return jsonify(edges), 201
+    newEdge = {"loose" : request.json['loose'], "win": request.json['win'], "user": request.json['user']}
+    insertEdge(newEdge)
+    return ("success"), 201
 
 @app.route('/moviedb/api/v1.0/toplist', methods=['GET'])
 @cross_origin()
 def toplist():
+    edges = findEdge(uid)
     if len(edges) != 0:
         tlist = pagerank(edges)[0]
         sortedtlist = sorted(tlist.items(), key=operator.itemgetter(1), reverse=True)
@@ -56,4 +51,9 @@ def toplist():
             outmovs.append(temp)
 
         return jsonify(outmovs)
-    else: return jsonify({'list': []})
+    else: return jsonify({})
+
+@app.route('/moviedb/api/v1.0/moviepct', methods=['GET'])
+@cross_origin()
+def pctMovie():
+    return jsonify(pctEdge(request.args.get('mov1'), request.args.get('mov2')))
